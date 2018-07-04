c21c66b15cc67712e4df6f1e3ebb7b1aa66893c7
王瑶(滴滴研究院) <yoyowangyao_i@didichuxing.com>
2018-07-04 12:43:51 +0800

    create-react-app 开发的fangQQ音乐移动端应用

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import ''
+// import App from './components/App';
+import Root from './components/Root';
+import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
